eb883815197ece5ebd7da84f96490dd9b82193a4
Eren Yatkin <eren.yatkin@bitaksi.com>
2020-07-23 20:51:30 +0300

    initial project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+    res.json({
+        test: 'test',
+    });
+});
+
+module.exports = router;
