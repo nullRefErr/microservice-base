3f82e7983d769fd7c4259d44645b3329e7c03d4d
Eren Yatkin <eren.yatkin@bitaksi.com>
2020-07-29 14:28:40 +0300

    mongoose connection established

--- loaders/index.js
+++ loaders/index.js
@@ -1,9 +1,11 @@
 const serverLoader = require('./server');
 const routesLoader = require('./route');
+const mongoLoader = require('./mongo');
 
 module.exports.init = ({ applicationServer }) => {
     const { httpServer } = serverLoader({ applicationServer });
     routesLoader({ applicationServer });
+    mongoLoader.init();
 
     return { httpServer };
 };
