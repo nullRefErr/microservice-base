0d29bc264f40bab07ff4eb3f02375eded5a21b8f
Eren Yatkin <eren.yatkin@bitaksi.com>
2020-08-31 17:35:02 +0300

    docker config files are added

--- config/index.js
+++ config/index.js
@@ -2,12 +2,10 @@ module.exports = {
     PORT: parseInt(process.env.PORT) || 3000,
     DATABASE: {
         HOST: process.env.DATABASE_HOST,
-        PORT: process.env.DATABASE_PORT,
     },
     CACHESTORE: {
         REDIS_CACHE: {
             HOST: process.env.REDIS_CACHE_HOST,
-            PORT: parseInt(process.env.REDIS_CACHE_PORT),
         },
     },
 };
