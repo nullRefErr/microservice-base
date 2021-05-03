ff288f0fb0479c3ed00ef82e2c57068ccd709171
eren <yatkineren@gmail.com>
2021-05-03 23:43:32 +0300

    routes folder bounded to a parameter and checked before initialize

---
+++ index.js
@@ -0,0 +1,20 @@
+const lib = require('./lib/bin/www');
+
+lib.Start({
+    ROUTES_FOLDER: './routes',
+    APP_PORT: 1338,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_IS_ENABLED: true,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_PORT: Number.parseInt(process.env.EMAIL_PORT),
+    EMAIL_SEND_LOCALLY: true,
+    EMAIL_USER: process.env.EMAIL_USER,
+    MONGO_HOST: process.env.DATABASE_HOST,
+    MONGO_IS_ENABLED: true,
+    REDIS_HOST: process.env.REDIS_CACHE_HOST,
+    REDIS_IS_ENABLED: true,
+    SES_API_VERSION: process.env.SES_API_VERSION,
+    SES_REGION: process.env.SES_REGION,
+    SES_USER_ACCESS_KEY_ID: process.env.SES_USER_ACCESS_KEY_ID,
+    SES_USER_SCREET_ACCESS_KEY: process.env.SES_USER_SCREET_ACCESS_KEY,
+});
